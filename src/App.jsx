ed0300c3ef44d4efbf9426b5cf23a6d061a0cf0e
Maxycarp89 <maxicostilla21@gmail.com>
2023-01-16 23:03:33 -0300

    add component hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "./Components/Hero";
 import NavBar from "./Components/NavBar";
 
 
 function App() {
   return <div>
     <NavBar />
+    <Hero />
   </div>;
 }
 
