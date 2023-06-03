a2c6880ba32ec3c7e2238cce6dd9080c633a7a8b
maxycarp89 <maxicostilla21@gmail.com>
2023-06-03 08:50:20 -0300

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import Analytics from "./Components/Analytics";
 import Hero from "./Components/Hero";
-import NavBar from "./Components/NavBar";
-import Newsletter from "./Components/Newsletter";
 import Cards from "./Components/Cards";
-import Footer from "./Components/Footer";
 
 function App() {
-  return <div>
-    <NavBar />
-    <Hero />
-    <Analytics />
-    <Newsletter />
-    <Cards />
-    <Footer />
-  </div>;
+  return (
+    <div>
+      <Hero />
+      <Cards />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Single from '../assets/single.png'
-import Double from '../assets/double.png'
-import Triple from '../assets/triple.png'
+
 
 
 const Cards = () => {
@@ -9,40 +7,55 @@ const Cards = () => {
     <div className="w-full py-[10rem] px-4 bg-white">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
         <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-500">
-            <img className="w-20 mx-auto mt-[-3rem] bg-white" src={Single} alt="/" />
-            <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
-            <p className="text-center text-4xl font-bold">%149</p>
+           
+            <h2 className="text-2xl font-bold text-center py-8">logo</h2>
+            <p className="text-center text-4xl font-bold">lorem ipsum</p>
             <div className="text-center font-medium">
-                <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
-                <p className="py-2 border-b mx-8">1 Granted User</p>
-                <p className="py-2 border-b mx-8"> Send up to 2 GB</p>
+          
             </div>
             <button className='bg-[#00df9a] w-[200px] rounded-md font-medium mx-auto my-6 px-6 py-3 text-black'>Start Trial</button>
         </div>
-        <div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-500">
-            <img className="w-20 mx-auto mt-[-3rem] bg-transparent" src={Double} alt="/" />
-            <h2 className="text-2xl font-bold text-center py-8">Double Users</h2>
-            <p className="text-center text-4xl font-bold">%149</p>
-            <div className="text-center font-medium">
-                <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
-                <p className="py-2 border-b mx-8">1 Granted User</p>
-                <p className="py-2 border-b mx-8"> Send up to 2 GB</p>
-            </div>
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-500">
+           
+            <h2 className="text-2xl font-bold text-center py-8">logo</h2>
+            <p className="text-center text-4xl font-bold">lorem ipsum</p>
+           
             <button className='text-[#00df9a] w-[200px] rounded-md font-medium mx-auto my-6 px-6 py-3 bg-black'>Start Trial</button>
         </div>
         <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-500">
-            <img className="w-20 mx-auto mt-[-3rem] bg-white" src={Triple} alt="/" />
-            <h2 className="text-2xl font-bold text-center py-8">Triple Users</h2>
-            <p className="text-center text-4xl font-bold">%149</p>
-            <div className="text-center font-medium">
-                <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
-                <p className="py-2 border-b mx-8">1 Granted User</p>
-                <p className="py-2 border-b mx-8"> Send up to 2 GB</p>
-            </div>
+            
+            <h2 className="text-2xl font-bold text-center py-8">logo</h2>
+            <p className="text-center text-4xl font-bold">lorem ipsum</p>
+            
             <button className='bg-[#00df9a] w-[200px] rounded-md font-medium mx-auto my-6 px-6 py-3 text-black'>Start Trial</button>
         </div>
       </div>
-      
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-500">
+           
+            <h2 className="text-2xl font-bold text-center py-8">logo</h2>
+            <p className="text-center text-4xl font-bold">lorem ipsum</p>
+            <div className="text-center font-medium">
+          
+            </div>
+            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium mx-auto my-6 px-6 py-3 text-black'>Start Trial</button>
+        </div>
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-500">
+           
+            <h2 className="text-2xl font-bold text-center py-8">logo</h2>
+            <p className="text-center text-4xl font-bold">lorem ipsum</p>
+           
+            <button className='text-[#00df9a] w-[200px] rounded-md font-medium mx-auto my-6 px-6 py-3 bg-black'>Start Trial</button>
+        </div>
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-500">
+            
+            <h2 className="text-2xl font-bold text-center py-8">logo</h2>
+            <p className="text-center text-4xl font-bold">lorem ipsum</p>
+            
+            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium mx-auto my-6 px-6 py-3 text-black'>Start Trial</button>
+        </div>
+      </div>
+
     </div>
   );
 };
